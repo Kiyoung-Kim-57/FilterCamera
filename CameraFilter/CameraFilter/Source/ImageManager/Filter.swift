cd37ef2f286cb1d692439d4327b00461c81479e5
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-21 14:05:12 +0900

    feat/#1 :: Define Filter Enum

---
+++ CameraFilter/CameraFilter/Source/ImageManager/Filter.swift
@@ -0,0 +1,22 @@
+import Foundation
+import CoreImage
+
+enum Filter {
+    case photoEffectTransfer
+    case photoEffectTonal
+    case randomGenerator
+    case multiplyBlendMode
+    case vignette
+    case sepiaTone
+    
+    var ciFilter: CIFilter {
+        switch self {
+        case .photoEffectTransfer: CIFilter(name: "CIPhotoEffectTransfer")!
+        case .photoEffectTonal: CIFilter(name: "CIPhotoEffectTonal")!
+        case .randomGenerator: CIFilter(name: "CIRandomGenerator")!
+        case .multiplyBlendMode: CIFilter(name: "CIMultiplyBlendMode")!
+        case .vignette: CIFilter(name: "CIVignette")!
+        case .sepiaTone: CIFilter(name: "CISepiaTone")!
+        }
+    }
+}
