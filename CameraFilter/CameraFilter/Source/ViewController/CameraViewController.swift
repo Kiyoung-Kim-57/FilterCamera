881f310d91f2d3137cc700f4f2ed8fa2285dc4c6
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-21 14:09:58 +0900

    feat/#1 :: Make Camera View, VC

---
+++ CameraFilter/CameraFilter/Source/ViewController/CameraViewController.swift
@@ -0,0 +1,72 @@
+import AVFoundation
+import UIKit
+import Combine
+
+final class CameraViewController: UIViewController {
+    private var cancellables = Set<AnyCancellable>()
+  
+    private let photoRoomBottomView: CameraBottomView
+
+    init(cameraBottomView: CameraBottomView) {
+        self.photoRoomBottomView = cameraBottomView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        addViews()
+        setupConstraints()
+        configureUI()
+        bindInput()
+        bindOutput()
+    }
+    
+    public func addViews() {
+        [photoRoomBottomView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    public func setupConstraints() {
+        photoRoomBottomView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(Constants.bottomViewHeight)
+        }
+    }
+    
+    public func configureUI() {
+    }
+    
+    public func bindInput() {
+        photoRoomBottomView.cameraButtonTapped
+            .sink { [weak self] _ in
+            }
+            .store(in: &cancellables)
+    }
+    
+    public func bindOutput() {
+    }
+}
+
+extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    }
+}
+
+extension CameraViewController {
+    private enum Constants {
+        static let bottomViewHeight: CGFloat = 80
+        static let navigationHeight: CGFloat = 48
+        static let circleButtonSize: CGSize = CGSize(width: 52, height: 52)
+        static let micButtonBottomSpacing: CGFloat = -4
+        static let micButtonLeadingSpacing: CGFloat = 16
+    }
+}
