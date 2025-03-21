2e0cb431e4d8f64813714ceeaa57668287e012de
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-21 19:23:35 +0900

    feat/#3 ::  Add takePhoto function

--- CameraFilter/CameraFilter/Source/Camera/CameraManager.swift
+++ CameraFilter/CameraFilter/Source/Camera/CameraManager.swift
@@ -7,6 +7,7 @@ import CoreImage
 final class CameraManager {
     private var captureSession = AVCaptureSession()
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    private var capturedImage: CIImage?
     
     func setupCamera(preset: AVCaptureSession.Preset = .photo, view: UIView) {
         self.captureSession.sessionPreset = preset
@@ -44,6 +45,14 @@ final class CameraManager {
         filter?.setValue(ciImage, forKey: kCIInputImageKey)
         guard let output = filter?.outputImage else { return }
         
+        self.capturedImage = output
         context.render(output, to: pixelBuffer)
+    }
+    
+    // Get Photo from camera
+    func takePhoto(scale: CGFloat = 1.0, orientation: UIImage.Orientation = .up) -> UIImage? {
+        guard let ciImage = self.capturedImage else { return nil }
+        
+        return UIImage(ciImage: ciImage, scale: scale, orientation: orientation)
     }
 }
