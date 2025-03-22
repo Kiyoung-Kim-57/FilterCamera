4e3b454d58c3a757f5ee02bcd5db4c4075e92efa
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-22 23:03:07 +0900

    feat/#3 :: Change RootViewController

--- CameraFilter/CameraFilter/App/SceneDelegate.swift
+++ CameraFilter/CameraFilter/App/SceneDelegate.swift
@@ -15,8 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let cameraBottomView = CameraBottomView()
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = CameraViewController(cameraBottomView: cameraBottomView)
         window?.makeKeyAndVisible()
     }
 }
