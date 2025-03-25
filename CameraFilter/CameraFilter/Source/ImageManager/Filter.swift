4216445b7bd9736fb58d83a0d6b2b614af8436c4
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-26 00:32:02 +0900

    feat/#5 :: Change Filter enum cases

--- CameraFilter/CameraFilter/Source/ImageManager/Filter.swift
+++ CameraFilter/CameraFilter/Source/ImageManager/Filter.swift
@@ -1,21 +1,8 @@
 import CoreImage
 
-enum Filter {
-    case photoEffectTransfer
-    case photoEffectTonal
-    case randomGenerator
-    case multiplyBlendMode
-    case vignette
-    case sepiaTone
-    
-    var ciFilter: CIFilter {
-        switch self {
-        case .photoEffectTransfer: CIFilter(name: "CIPhotoEffectTransfer")!
-        case .photoEffectTonal: CIFilter(name: "CIPhotoEffectTonal")!
-        case .randomGenerator: CIFilter(name: "CIRandomGenerator")!
-        case .multiplyBlendMode: CIFilter(name: "CIMultiplyBlendMode")!
-        case .vignette: CIFilter(name: "CIVignette")!
-        case .sepiaTone: CIFilter(name: "CISepiaTone")!
-        }
-    }
+enum Filter: String {
+    case original
+    case vintage
+    case noir
+    case bloom
 }
