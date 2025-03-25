90116aacb26135c774c9fa3daf99726eaac9d1ef
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-26 00:30:52 +0900

    feat/#5 :: Replace Filters with Core Image CIFilterBuiltins

--- CameraFilter/CameraFilter/Source/ImageManager/ImageFilterManager.swift
+++ CameraFilter/CameraFilter/Source/ImageManager/ImageFilterManager.swift
@@ -1,7 +1,21 @@
 import CoreImage
+import CoreImage.CIFilterBuiltins
 
 final class ImageFilterManager {
     let context = CIContext()
+    
+    func applyFilters(_ ciImage: CIImage, filter: Filter) -> CIImage? {
+        switch filter {
+        case .original:
+            return ciImage
+        case .vintage:
+            return applyVintageFilter(ciImage: ciImage)
+        case .noir:
+            return applyNoirFilter(ciImage: ciImage)
+        case .bloom:
+            return applyBloomFilter(ciImage: ciImage)
+        }
+    }
     
     func applyVintageFilter(ciImage: CIImage) -> CIImage? {
         @FilterBuilder
@@ -14,26 +28,45 @@ final class ImageFilterManager {
         
         return filteredImage
     }
+    
+    func applyNoirFilter(ciImage: CIImage) -> CIImage? {
+        @FilterBuilder
+        var filteredImage: CIImage? {
+            noirFilter(ciImage: ciImage)
+            vignetteFilter(intensity: 1.2, radius: 3.0)
+        }
+        return filteredImage
+    }
+    
+    func applyBloomFilter(ciImage: CIImage) -> CIImage? {
+        @FilterBuilder
+        var filteredImage: CIImage? {
+            photoEffectTransfer(ciImage: ciImage)
+            sepiaFilter()
+            bloomFilter()
+        }
+        return filteredImage
+    }
 }
 
 // MARK: Filters
 extension ImageFilterManager {
     private func photoEffectTransfer(ciImage: CIImage = CIImage()) -> CIFilter {
-        let filter = Filter.photoEffectTransfer.ciFilter
+        let filter = CIFilter.photoEffectTransfer()
         filter.setValue(ciImage, forKey: kCIInputImageKey)
         return filter
     }
     
     //덮어씌울 노이즈 레이어 생성
     private func noiseLayer(rect: CGRect) -> CIImage? {
-        let filter = Filter.randomGenerator.ciFilter
+        let filter = CIFilter.randomGenerator()
         let noiseImage = filter.outputImage?.cropped(to: rect)
         return noiseImage
     }
     
     //노이즈 필터
     private func noiseFilter(ciImage: CIImage = CIImage(), rect: CGRect) -> CIFilter {
-        let filter = Filter.multiplyBlendMode.ciFilter
+        let filter = CIFilter.multiplyBlendMode()
         filter.setValue(ciImage, forKey: kCIInputImageKey)
         filter.setValue(noiseLayer(rect: rect), forKey: kCIInputBackgroundImageKey)
         return filter
@@ -41,7 +74,7 @@ extension ImageFilterManager {
     
     // 비네트필터
     private func vignetteFilter(ciImage: CIImage = CIImage(), intensity: CGFloat, radius: CGFloat) -> CIFilter {
-        let filter = Filter.vignette.ciFilter
+        let filter = CIFilter.vignette()
         filter.setValue(ciImage, forKey: kCIInputImageKey)
         filter.setValue(intensity, forKey: kCIInputIntensityKey)
         filter.setValue(radius, forKey: kCIInputRadiusKey)
@@ -50,15 +83,35 @@ extension ImageFilterManager {
     
     // Tonal, 흑백 느와르 필터
     private func tonalFilter(ciImage: CIImage = CIImage()) -> CIFilter {
-        let filter = Filter.photoEffectTonal.ciFilter
+        let filter = CIFilter.photoEffectTonal()
         filter.setValue(ciImage, forKey: kCIInputImageKey)
         return filter
     }
     
     // SepiaTone
     private func sepiaFilter(ciImage: CIImage = CIImage()) -> CIFilter {
-        let filter = Filter.sepiaTone.ciFilter
+        let filter = CIFilter.sepiaTone()
         filter.setValue(ciImage, forKey: kCIInputImageKey)
+        return filter
+    }
+    
+    //Noir
+    private func noirFilter(ciImage: CIImage = CIImage()) -> CIFilter {
+        let filter = CIFilter.photoEffectNoir()
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        return filter
+    }
+    
+    // Bloom
+    private func bloomFilter(
+        ciImage: CIImage = CIImage(),
+        radius: CGFloat = 10,
+        intensity: CGFloat = 0.5
+    ) -> CIFilter {
+        let filter = CIFilter.bloom()
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        filter.setValue(radius, forKey: kCIInputRadiusKey)
+        filter.setValue(intensity, forKey: kCIInputIntensityKey)
         return filter
     }
 }
