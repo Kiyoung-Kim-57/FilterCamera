4c65cda982d37249b927514a789c0ce60c3a296d
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-26 00:32:48 +0900

    feat/#5 :: Make FilterCollectionViewCell

--- CameraFilter/CameraFilter/Source/View/FilterCollectionViewCell.swift
+++ CameraFilter/CameraFilter/Source/View/FilterCollectionViewCell.swift
@@ -4,4 +4,43 @@ import SnapKit
 final class FilterCollectionViewCell: UICollectionViewCell {
     private let imageView = UIImageView()
     private let filterLabel = UILabel()
+    private var sampleImage = UIImage(resource: .sample)
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addViews()
+        setupConstraints()
+        configureUI()
+    }
+    
+    private func addViews() {
+        [imageView, filterLabel].forEach { addSubview($0) }
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(filterLabel.snp.top)
+        }
+        
+        filterLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func configureUI() {
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    func configureCell() {
+        
+    }
 }
