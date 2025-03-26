e5125d4f16e1f3ed0241c0c066da10464dd30612
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-27 00:58:48 +0900

    feat/#5 :: Add FilterCollectionView Dependency

--- CameraFilter/CameraFilter/App/SceneDelegate.swift
+++ CameraFilter/CameraFilter/App/SceneDelegate.swift
@@ -7,13 +7,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+        let filterCollectionViewController = FilterCollectionViewController(collectionViewLayout: UICollectionViewLayout())
         let imageFilter = ImageFilterManager()
         let cameraManager = CameraManager(imageFilterManager: imageFilter)
         let cameraBottomView = CameraBottomView()
         let cameraViewModel = CameraViewModel(cameraManager: cameraManager)
         let photoViewModel = PhotoViewModel(cameraManager: cameraManager)
         let cameraViewController = CameraViewController(
+            filterCollectionViewController: filterCollectionViewController,
             cameraBottomView: cameraBottomView,
             cameraViewModel: cameraViewModel,
             photoViewModel: photoViewModel
