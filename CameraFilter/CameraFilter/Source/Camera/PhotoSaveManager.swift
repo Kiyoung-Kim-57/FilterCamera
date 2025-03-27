08e6d543a6cee16a7f24eda281203e6c3bdacf7f
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-03-27 21:55:49 +0900

    feat/#9 :: Add return value(bool) in savePhoto function

--- CameraFilter/CameraFilter/Source/Camera/PhotoSaveManager.swift
+++ CameraFilter/CameraFilter/Source/Camera/PhotoSaveManager.swift
@@ -2,15 +2,20 @@ import Photos
 import UIKit
 
 final class PhotoSaveManager {
-    static func savePhoto(image: UIImage) {
+    @discardableResult
+    static func savePhoto(image: UIImage) -> Bool {
+        var result = true
         PHPhotoLibrary.shared().performChanges {
             PHAssetChangeRequest.creationRequestForAsset(from: image)
         } completionHandler: { success, error in
             if let error {
+                result = false
                 debugPrint("Error occured while saving photo: \(error.localizedDescription)")
             } else {
+                result = true
                 debugPrint("Photo saved successfully!")
             }
         }
+        return result
     }
 }
